d1f4953f55ab41cbc9e31078197a69d43d7d6846
Edgardo-Medina-72 <edgardoalm72@gmail.com>
2023-06-01 16:02:49 -0300

    Nuevo archivo html con bootstrap

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ window.onscroll = () => {
     document.querySelector(".contenedor").classList.remove("mostrar");
   }
 };
+
+const $form = document.querySelector("#form");
+
+$form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  console.log(form.get("nombre"));
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    this.reset();
+    alert("Gracias");
+  }
+}
