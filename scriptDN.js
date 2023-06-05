6472fe30607b0a5b31c2923f7e370466691087f2
Edgardo-Medina-72 <edgardoalm72@gmail.com>
2023-06-05 20:33:33 -0300

    actualizacion

---
+++ scriptDN.js
@@ -0,0 +1,46 @@
+//cambio de tema
+function temaDia() {
+  document.querySelector("nav").classList.remove("navbar-dark", "bg-dark");
+  document.querySelector("nav").classList.add("navbar-light", "bg-light");
+  console.log("dia");
+  /* document
+    .querySelectorAll("#marco")
+    .forEach((mrc) => mrc.classList.add("marcoDia")); */
+  /* document
+    .querySelectorAll("#relleno")
+    .forEach((rlln) => rlln.classList.add("rellenoDia")); */
+  /* document
+    .querySelectorAll(".botonMenu")
+    .forEach((btn) => btn.classList.add("botonMenuDia")); */
+  /* document
+    .querySelectorAll(".tarjetaM")
+    .forEach((card) => card.classList.add("tarjetaMDia")); */
+  localStorage.setItem("tema", "dia");
+}
+/* ------------------------------------------------------------ */
+function temaNoche() {
+  document.querySelector("nav").classList.add("navbar-dark", "bg-dark");
+  document.querySelector("nav").classList.remove("navbar-light", "bg-light");
+  console.log("noche");
+  /* document
+    .querySelectorAll("#marco")
+    .forEach((mrc) => mrc.classList.remove("marcoDia")); */
+  /* document
+    .querySelectorAll("#relleno")
+    .forEach((rlln) => rlln.classList.remove("rellenoDia")); */
+  /* document
+    .querySelectorAll(".botonMenu")
+    .forEach((btn) => btn.classList.remove("botonMenuDia")); */
+  /* document
+    .querySelectorAll(".tarjetaM")
+    .forEach((card) => card.classList.remove("tarjetaMDia")); */
+  localStorage.setItem("tema", "noche");
+}
+
+let infoTema = localStorage.getItem("tema");
+
+if (infoTema == "dia") {
+  temaDia();
+} else {
+  temaNoche();
+}
